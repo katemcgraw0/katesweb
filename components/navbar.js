71b2759378ae340a3ce7e39f1e9b53b74583b3e9
katemcg <katemcg@umich.edu>
2023-07-13 21:46:23 -0400

    Layout and format setup

---
+++ components/navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+
+const Navbar = () => {
+  const router = useRouter();
+  const isHomePage = router.pathname === '/';
+  const isProjectsPage = router.pathname == '/projects';
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex items-center space-x-4">
+              <a className=" text-4xl font-bold text-green5 ">Katherine (Kate) McGraw</a>
+            </div>
+          </div>
+          <div className="flex px-4">
+            <Link legacyBehavior href="/">
+            <a
+                className={`block text-4xl font-bold text-white rounded-l-lg px-4 ${isHomePage ? 'bg-green5' : 'bg-green4'
+                } hover:bg-green5 transition-colors`}
+              >
+                Home
+              </a>
+            </Link>
+            <Link legacyBehavior href="/projects">
+              <a className={`block text-4xl font-bold text-white rounded-r-lg px-4 ${isProjectsPage ? 'bg-green5' : 'bg-green4'
+                } hover:bg-green5 transition-colors`}>Projects</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+
+
+export default Navbar;
---
+++ pages/projects.js
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../components/layout'
+
+export default function Home() {
+    return (
+      <Layout>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow">
+          {/* Toolbar */}
+          
+  
+          {/* Page Content */}
+          {/* Add your content here */}
+        </main>
+  
+        {/* Footer */}
+        <footer className="bg-gray-800 text-white p-4">
+          <div className="flex justify-center">
+            <Link legacyBehavior href="/about">
+              <a className="mr-4">About</a>
+            </Link>
+            <Link legacyBehavior href="/contact">
+              <a>Contact</a>
+            </Link>
+          </div>
+        </footer>
+      </div>
+      </Layout>
+    );
+  }

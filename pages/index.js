2f877755316f5f1ccadbde81e7485a3ba4c049cc
katemcg <katemcg@umich.edu>
2024-07-08 17:45:12 -0500

    Minor fixes to icon hover components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Layout from '../components/layout';
-import {ReactTyped} from 'react-typed';
+import { ReactTyped } from 'react-typed';
 
 const IconWithText = ({ src, alt, text }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -8,10 +8,10 @@ const IconWithText = ({ src, alt, text }) => {
   return (
     <div
       className="icon-wrapper flex items-center"
-      onMouseEnter={() => setIsHovered(true)}
+     
       //onMouseLeave={() => setIsHovered(false)}
     >
-      <img src={src} alt={alt} className="icon rounded-full w-1/8" />
+      <img  onMouseEnter={() => setIsHovered(true)} src={src} alt={alt} className="icon rounded-full w-1/8" />
       {isHovered && (
         <ReactTyped
           className="typing-text ml-2 custom-typed"
@@ -21,7 +21,6 @@ const IconWithText = ({ src, alt, text }) => {
           cursorChar="|"
         />
       )}
-
     </div>
   );
 };
@@ -45,20 +44,19 @@ export default function Home() {
               </div>
               {/* Text Box */}
               <div className="lg:w-1/2 lg:pl-8 flex items-center ">
-                <div className="bg-green3 p-4 text-white rounded-xl w-full ">
+                <div className="bg-green3 p-4 text-white rounded-xl w-full hover:transform hover:scale-105 transition-transform duration-300">
                   <h2 className="font-custom sm:text-5xl text-2xl font-bold mb-4">
                     Hi! My name's Kate McGraw.
                   </h2>
                   <p className="sm:text-2xl text-lg mb-4 font-bodyCust">
-                    Hover below to learn more about me!
+                    Hover on the icons below to learn more about me!
                   </p>
                   <div className="flex flex-col space-y-4 font-custom text-xl">
-                  <IconWithText src="/icons/coding-icon.png" alt="Coding Icon" text="I am an aspiring Software Engineer." />
-                    <IconWithText src="/icons/home-icon.png" alt="Home Icon" text="I am from San Diego, CA." />
-                    <IconWithText src="/icons/school-icon.png" alt="School Icon" text="I am studying CS at UMich. Go Blue!!!!" />
-                    <IconWithText src="/icons/teaching-icon.png" alt="Teacher Icon" text="I am a TA for a CS Theory class, EECS 376." />
-                    <IconWithText src="/icons/hobbies-icon.png" alt="Hobbies Icon" text="My hobbies include hiking, listening to a lot of music, and slacklining." />
-                    
+                    <IconWithText src="/icons/coding-icon.png" alt="Coding Icon" text="//I am an aspiring Software Engineer, currently interning at Walmart Global Tech working with the Data Ventures team." />
+                    <IconWithText src="/icons/home-icon.png" alt="Home Icon" text="//I am from San Diego, CA-—born and raised." />
+                    <IconWithText src="/icons/school-icon.png" alt="School Icon" text="//I am studying CS at UMich. Go Blue!!!!" />
+                    <IconWithText src="/icons/teaching-icon.png" alt="Teacher Icon" text="//I am a TA for a CS Theory class,  <a href='https://eecs376.org/' target='_blank' rel='noopener noreferrer className ='text-blue' '>EECS 376</a>, and I love sharing my passion for learning with others." />
+                    <IconWithText src="/icons/hobbies-icon.png" alt="Hobbies Icon" text="//My hobbies include hiking, listening to a lot of music, and slacklining." />
                   </div>
                 </div>
               </div>
